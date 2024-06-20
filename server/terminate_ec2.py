d1d16269c059f0f026097b3e35f1e8635b45ee5a
ariskumara <aris.kumara@gmail.com>
2024-06-20 16:53:05 +0700

    Create terminate_ec2.py

---
+++ server/terminate_ec2.py
@@ -0,0 +1,35 @@
+""" Lambda to delete ec2-instances """
+import boto3
+import os
+
+REGION =  os.environ['REGION']
+
+EC2 = boto3.client('ec2', region_name=REGION)
+
+def lambda_handler(event, context):
+    
+    serverid = '' 
+    
+    try:
+        if (event['queryStringParameters']) and (event['queryStringParameters']['serverid']) and (
+                event['queryStringParameters']['serverid'] is not None):
+            serverid = event['queryStringParameters']['serverid']
+            
+        instance = EC2.terminate_instances(
+        InstanceIds=[
+            serverid 
+            ],
+        )
+        
+    except KeyError:
+        print('No server id')
+    
+    res = {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "*/*"
+        },
+        "body": "The server id, " + serverid + " has been terminated!"
+    }
+    
+    return res
